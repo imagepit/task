c9a1624e2fdceadd979ef7bfda581ef6cc9e0dc5
Ryosuke Takahashi <r-takahashi@image-pit.com>
2024-04-30 16:58:10 +0900

    first commit

---
+++ src/test/java/com/msa/task/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.msa.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class HelloControllerTest {
+  @Autowired
+  private HelloController helloController;
+
+  @Test
+  public void contextLoads() throws Exception {
+    assertThat(helloController).isNotNull();
+  }
+
+  @Test
+  public void testHello() {
+    assertThat(helloController.hello()).isEqualTo("Hello, World!");
+  }
+}
